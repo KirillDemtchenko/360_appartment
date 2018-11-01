b9ac5c3dc5a96867e0a2ee78d54fd3b80b88ab1b
KirillDemtchenko <30822663+KirillDemtchenko@users.noreply.github.com>
2018-11-02 00:53:44 +0300

    init commit via upload from pc

---
+++ SwitchScenes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SwitchScenes : MonoBehaviour
+{
+    public string FirstScene = "FirstScene";
+		public string SecondScene = "SecondScene";
+    Scene m_Scene;
+
+    void Update()
+    {
+        m_Scene = SceneManager.GetActiveScene();
+
+				if (Input.GetKeyDown (KeyCode.Space) || Input.touchCount == 1) {
+					if (m_Scene.name == FirstScene)
+					{
+							SceneManager.LoadScene(SecondScene);
+					}
+	
+					if (m_Scene.name == SecondScene)
+					{
+							SceneManager.LoadScene(FirstScene);
+					}
+				}
+					
+    }
+}
